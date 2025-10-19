27a981688eade44fadaaec53e7d70faddf18c43f
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-10-19 21:15:59 +0100

    task finish

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        if not Animal.alive:
+            return "No animals are alive 🐾"
+        return (f"{{"
+                f"Name: {self.name}, Health: {self.health},"
+                f" Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    @classmethod
+    def bite(cls, herbivore: Herbivore) -> str | None:
+        if isinstance(herbivore, cls) or herbivore.hidden is True:
+            return f"{cls} cannot bite {herbivore} rabbit"
+        else:
+            herbivore.health -= 50
+            if herbivore.health <= 0:
+                Animal.alive.remove(herbivore)
